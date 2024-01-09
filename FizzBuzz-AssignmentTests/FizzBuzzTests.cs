f48cca8d8aa9b6178e59a84065562e3bfca5ceab
Andrii Stets <andrii.stets@saturnvisual.com>
2024-01-09 11:10:19 +0000

    Add NUnit Test For this

---
+++ FizzBuzz-AssignmentTests/FizzBuzzTests.cs
@@ -0,0 +1,63 @@
+﻿using FizzBuzz_Assignment.DomainModels;
+
+namespace FizzBuzz_AssignmentTests;
+
+[TestFixture]
+public class FizzBuzzTests
+{
+    [Test]
+    public void FizzBuzz_ShouldReturnFizz_WhenDivisibleBy3()
+    {
+        // Arrange
+        var fizzAction = new FizzAction();
+        var fizzBuzz = new FizzBuzz(new[] { fizzAction });
+
+        // Act
+        var result = fizzBuzz.Execute(3);
+
+        // Assert
+        Assert.AreEqual("Fizz", result);
+    }
+
+    [Test]
+    public void FizzBuzz_ShouldReturnBuzz_WhenDivisibleBy5()
+    {
+        // Arrange
+        var buzzAction = new BuzzAction();
+        var fizzBuzz = new FizzBuzz(new[] { buzzAction });
+
+        // Act
+        var result = fizzBuzz.Execute(5);
+
+        // Assert
+        Assert.AreEqual("Buzz", result);
+    }
+
+    [Test]
+    public void FizzBuzz_ShouldReturnFizzBuzz_WhenDivisibleByBoth3And5()
+    {
+        // Arrange
+        var fizzBuzzAction = new FizzBuzzAction();
+        var fizzBuzz = new FizzBuzz(new[] { fizzBuzzAction });
+
+        // Act
+        var result = fizzBuzz.Execute(15);
+
+        // Assert
+        Assert.AreEqual("FizzBuzz", result);
+    }
+
+    [Test]
+    public void FizzBuzz_ShouldReturnNumber_WhenNotDivisibleBy3Or5()
+    {
+        // Arrange
+        var standardAction = new StandardAction(7);
+        var fizzBuzz = new FizzBuzz(new[] { standardAction });
+
+        // Act
+        var result = fizzBuzz.Execute(7);
+
+        // Assert
+        Assert.AreEqual("7", result);
+    }
+}
---
+++ FizzBuzz-AssignmentTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FizzBuzz_AssignmentTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
